61c6b444d092ffaa73265e1193398409a6e73c82
kc1212 <kc1212@users.noreply.github.com>
2023-07-22 11:54:36 +0200

    Implement up to h; untested

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,103 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod primitive;
+use primitive::*;
+
+
+#[derive(Debug, Copy, Clone)]
+struct Param {
+    /// Dimension of the SSP
+    n: usize,
+    /// Number of parties
+    N: usize,
+    /// Parameter for cut and choose
+    M: usize,
+    /// Parameter for abort
+    A: usize,
 }
+
+impl Param {
+    fn default() -> Self {
+        Param {
+            n: 128,
+            N: 4,
+            M: 100,
+            A: 1 << 14,
+        }
+    }
+}
+
+struct Witness(Vec<u8>);
+struct Instance {
+    weights: Vec<u64>,
+    t: u64,
+}
+
+struct Prover {
+    witness: Witness,
+    instance: Instance,
+    mseed: [u8; BLOCK_SIZE],
+    iv: [u8; BLOCK_SIZE],
+    param: Param,
+}
+
+fn hash_witness_instance(witness: &Witness, instance: &Instance) -> [u8; BLOCK_SIZE] {
+    unimplemented!()
+}
+
+impl Prover {
+    fn new(witness: Witness, instance: Instance, mseed: [u8; BLOCK_SIZE], param: Param) -> Self {
+        let iv = hash_witness_instance(&witness, &instance);
+        // TODO check params, A needs to be a power of 2
+        Prover {
+            witness,
+            instance,
+            mseed,
+            iv,
+            param,
+        }
+    }
+
+    fn step1(&self) -> [u8; DIGEST_SIZE] {
+        let mseeds = prg_tree(&self.mseed, &self.iv, self.param.M);
+        let mut h1s = Vec::with_capacity(self.param.M);
+
+        for e in 0..self.param.M {
+            let rs = prg_bin(&mseeds[e], &self.iv, self.param.n);
+            let seeds_rhos = prg_tree(&mseeds[e], &self.iv, self.param.N * 2);
+            let (seeds, rhos): (Vec<_>, Vec<_>) = seeds_rhos.chunks_exact(2).map(|arr| {
+                (arr[0], arr[1])
+            }).unzip();
+            debug_assert_eq!(seeds.len(), self.param.N);
+            debug_assert_eq!(rhos.len(), self.param.N);
+
+            let r_shares: Vec<Vec<u64>> = seeds.iter().map(|seed| {
+                prg_u64(seed, &self.iv, self.param.n).iter().map(|x| {
+                    x % (self.param.A as u64)
+                }).collect()
+            }).collect();
+
+            let coms: Vec<_> = seeds.iter().zip(rhos.iter()).map(|(seed, rho)| {
+                commit(seed, &Opening(*rho))
+            }).collect();
+
+            // sum over the N vectors
+            let r_shares_sum: Vec<u64> = r_shares.iter().fold(vec![0u64; self.param.n], |acc, x| {
+                acc.into_iter().zip(x).map(|(a, b)| {
+                    a + b
+                }).collect()
+            });
+            let delta_rs: Vec<_> = rs.iter().zip(&r_shares_sum).map(|(r, share)| {
+                (*r as u64).wrapping_sub(*share)
+            }).collect();
+
+            let h1 = hash1(&delta_rs, &coms);
+            h1s.push(h1);
+        }
+        let h = hash2(&h1s);
+        h
+    }
+}
+
+struct FullTranscript {}
 
 #[cfg(test)]
 mod tests {
@@ -8,7 +105,5 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
     }
 }
---
+++ src/primitive.rs
@@ -0,0 +1,178 @@
+use aes::cipher::{KeyIvInit, StreamCipher, KeySizeUser, IvSizeUser};
+use sha3::{Digest, Sha3_256};
+use std::collections::VecDeque;
+
+pub(crate) const KEY_SIZE: usize = 16;
+pub(crate) const BLOCK_SIZE: usize = 16;
+pub(crate) const OPENING_SIZE: usize = 16;
+pub(crate) const DIGEST_SIZE: usize = 32;
+
+type Aes128Ctr = ctr::Ctr64BE<aes::Aes128>;
+// type Block = [u8; BLOCK_SIZE];
+// type Opening = [u8; OPENING_SIZE];
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Opening(pub [u8; OPENING_SIZE]);
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Commitment([u8; DIGEST_SIZE]);
+
+pub fn hash1(delta_rs: &[u64], coms: &[Commitment]) -> [u8; DIGEST_SIZE] {
+    let mut hasher = Sha3_256::new();
+    hasher.update("delta_rs");
+    hasher.update(&delta_rs.len().to_le_bytes());
+    for delta_r in delta_rs {
+        hasher.update(&delta_r.to_le_bytes());
+    }
+    hasher.update("coms----");
+    hasher.update(&coms.len().to_le_bytes());
+    for com in coms {
+        hasher.update(&com.0);
+    }
+
+    let result = hasher.finalize();
+    result.as_slice().try_into().unwrap()
+}
+
+pub fn hash2(h1s: &[[u8; DIGEST_SIZE]]) -> [u8; DIGEST_SIZE] {
+    let mut hasher = Sha3_256::new();
+    hasher.update("h1s-----");
+    hasher.update(&h1s.len().to_le_bytes());
+    for h1 in h1s {
+        hasher.update(h1);
+    }
+
+    let result = hasher.finalize();
+    result.as_slice().try_into().unwrap()
+}
+
+pub fn commit(value: &[u8], opening: &Opening) -> Commitment {
+    debug_assert_eq!(Sha3_256::output_size(), DIGEST_SIZE);
+    let mut hasher = Sha3_256::new();
+    hasher.update(opening.0);
+    hasher.update(value);
+    let result = hasher.finalize();
+    // TODO avoid copying, change to generic array?
+    Commitment(result.as_slice().try_into().unwrap())
+}
+
+pub fn verify(value: &[u8], opening: &Opening, commitment: &Commitment) -> bool {
+    let actual = commit(value, opening);
+    actual == *commitment
+}
+
+/// TODO: better to output vec of arrays instead of vec of bytes
+pub fn prg_aes_ctr(seed: &[u8; KEY_SIZE], iv: &[u8; BLOCK_SIZE], block_count: usize) -> Vec<u8> {
+    let mut cipher = Aes128Ctr::new(seed.into(), iv.into());
+    debug_assert_eq!(Aes128Ctr::key_size(), KEY_SIZE);
+    debug_assert_eq!(Aes128Ctr::iv_size(), BLOCK_SIZE);
+    let mut out = vec![0u8; block_count * BLOCK_SIZE];
+    cipher.apply_keystream(&mut out);
+    out
+}
+
+pub fn prg_u64(seed: &[u8; KEY_SIZE], iv: &[u8; BLOCK_SIZE], n: usize) -> Vec<u64> {
+    assert!(n >= 1);
+    let block_count = if (n*8 % BLOCK_SIZE) == 0 {
+        n*8 / BLOCK_SIZE
+    } else {
+        n*8 / BLOCK_SIZE + 1
+    };
+    let blocks = prg_aes_ctr(seed, iv, block_count);
+    debug_assert_eq!(blocks.len() % 8, 0);
+    let mut out = vec![0u64; n];
+    for i in (0..blocks.len()).step_by(8) {
+        out[i/8] = u64::from_le_bytes(blocks[i..i+8].try_into().expect("must be 8 bytes"));
+    }
+    out
+}
+
+pub fn prg_bin(seed: &[u8; KEY_SIZE], iv: &[u8; BLOCK_SIZE], n: usize) -> Vec<u8> {
+    assert!(n >= 1);
+    let block_count = n / BLOCK_SIZE + 1;
+    let blocks = prg_aes_ctr(seed, iv, block_count);
+    let mut out = vec![0u8; n];
+    let mut i = 0usize;
+    for block in blocks {
+        for shift in 0u8..8 {
+            out[i] = (block >> shift) & 1;
+            i += 1;
+            if i == n {
+                return out;
+            }
+        }
+    }
+    unreachable!()
+}
+
+pub fn prg_double(seed: &[u8; KEY_SIZE], iv: &[u8; BLOCK_SIZE]) -> ([u8; BLOCK_SIZE], [u8; BLOCK_SIZE]) {
+    let mut left = prg_aes_ctr(seed, iv, 2);
+    let right = left.split_off(BLOCK_SIZE);
+    (left.try_into().unwrap(), right.try_into().unwrap())
+}
+
+/// Easier to build an unbalanced tree when compared to the recursive method.
+pub fn prg_tree(seed: &[u8; KEY_SIZE], iv: &[u8; BLOCK_SIZE], n: usize) -> Vec<[u8; BLOCK_SIZE]> {
+    let mut out = VecDeque::with_capacity(n);
+    while out.len() < n {
+        if out.is_empty() {
+            // NOTE: this assumes the key size is the same as the block size
+            out.push_back(*seed);
+            continue;
+        }
+        let new_seed: [u8; KEY_SIZE] = out
+            .pop_front()
+            .expect("deque should be initialized here");
+        let (left, right) = prg_double(&new_seed, iv);
+        out.push_back(left);
+        out.push_back(right);
+    }
+    out.into()
+}
+
+#[test]
+fn test_commit() {
+    let value = [0u8, 1, 2, 3];
+    let opening = Opening([1u8; OPENING_SIZE]);
+    let commitment = commit(&value, &opening);
+    assert!(verify(&value, &opening, &commitment));
+
+    let bad_opening = Opening([2u8; OPENING_SIZE]);
+    assert!(!verify(&value, &bad_opening, &commitment));
+}
+
+#[test]
+fn test_prg() {
+    let seed = [0u8; KEY_SIZE];
+    let iv = [0u8; BLOCK_SIZE];
+    let out1 = prg_aes_ctr(&seed, &iv, 1);
+    assert_eq!(out1.len(), BLOCK_SIZE);
+
+    let out2 = prg_aes_ctr(&seed, &iv, 2);
+    assert_eq!(out2.len(), BLOCK_SIZE * 2);
+
+    let seed2 = [1u8; KEY_SIZE];
+    let out3 = prg_aes_ctr(&seed2, &iv, 1);
+    assert_ne!(out1, out3);
+
+    let out4 = prg_bin(&seed, &iv, 1);
+    assert_eq!(out4.len(), 1);
+    assert!(out4[0] == 0 || out4[0] == 1);
+
+    let out5 = prg_bin(&seed, &iv, BLOCK_SIZE * 8);
+    assert_eq!(out5.len(), BLOCK_SIZE * 8);
+    for b in out5 {
+        assert!(b == 0 || b == 1);
+    }
+}
+
+#[test]
+fn test_prg_tree() {
+    let seed = [0u8; KEY_SIZE];
+    let iv = [0u8; BLOCK_SIZE];
+    let out = prg_tree(&seed, &iv, 2);
+    assert_eq!(out.len(), 2);
+    assert_eq!(out[0].len(), BLOCK_SIZE);
+    assert_eq!(out[1].len(), BLOCK_SIZE);
+    assert_ne!(out[0], out[1]);
+}
